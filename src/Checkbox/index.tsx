1af9c54f0d91d598bf047183b56b625e87785006
Matt Wood <matt@cratebind.com>
2019-11-07 14:12:55 -0600

    Initial commit

---
+++ src/Checkbox/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+// import './styles.css';
+
+/**
+ * TODO:
+ * - Add `loading` prop that shows a spinner?
+ * - Add sizes
+ */
+
+// extend the native HTML attributes for nicer autocompletion
+export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // define the custom props we're going to be using
+  children: React.ReactNode
+  // add a question mark to make it an optional prop
+  disabled?: boolean
+  checked?: boolean
+  onChange: () => any
+  style: any
+}
+
+export const Checkbox = (props: CheckboxProps) => {
+  const { children, checked = false, onChange, ...rest } = props;
+
+  return (
+    <label data-ui-label>
+      <div data-ui-control-box data-ui-checked={checked} />
+      <input role="checkbox" type="checkbox" onChange={onChange} data-ui-visually-hidden {...rest} />
+      {children}
+    </label>
+  )
+}
