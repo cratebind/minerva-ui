d6fae01fd4871ef29f42b1c6df598e28c308f843
Matt Wood <matt@cratebind.com>
2019-11-07 23:42:16 -0600

    Change to use MDX, add custom CodeBlock components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import CodeBlock from '../src/CodeBlock';
+
+addParameters({
+  docs: {
+    components: {
+      code: CodeBlock,
+    },
+  },
+});
+
 
 // automatically import all files ending in *.stories.js and *.stories.mdx
 configure([
---
+++ src/CodeBlock.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+export declare const liveEditorStyle: {
+    fontSize: number;
+    marginBottom: number;
+    marginTop: number;
+    fontFamily: string;
+    borderRadius: number;
+    padding: number;
+};
+export declare const liveErrorStyle: {
+    fontFamily: string;
+    fontSize: number;
+    padding: string;
+    color: string;
+    backgroundColor: string;
+};
+declare const CodeBlock: {
+    ({ className, live, isManual, render, children, ...props }: any): JSX.Element;
+    defaultProps: {
+        mountStylesheet: boolean;
+    };
+    displayName: string;
+    __docgenInfo: {
+        "description": string;
+        "displayName": string;
+        "props": {};
+    };
+};
+export default CodeBlock;
