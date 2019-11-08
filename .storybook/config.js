143562fdfe938d5bd6a72e9f802ff68a8686027f
Matt Wood <matt@cratebind.com>
2019-11-07 21:23:02 -0600

    Add MDX and welcome page

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js and *.stories.mdx
+configure([
+  require.context('../stories', true, /\.stories\.js$/),
+  require.context('../stories', true, /\.stories\.mdx$/),
+], module);
+
