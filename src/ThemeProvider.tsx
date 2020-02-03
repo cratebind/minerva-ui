cf999035ce68cc7546c1ce6759093f0547207285
Matt Wood <matt@cratebind.com>
2020-02-03 09:57:28 -0600

    Add font name aliases

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -33,6 +33,18 @@ const exportedTheme = {
   colors: {
     primary: '#6979F8',
   },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '4rem',
+  },
 };
 
 export interface ProviderProps {
