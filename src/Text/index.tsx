d09c4f0c985b6939ba2466ab32cfca92943578a0
Matt Wood <matt@cratebind.com>
2020-02-03 13:28:15 -0600

    Update example, update Text component, update story headers

--- src/Text/index.tsx
+++ src/Text/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  background,
+  border,
+  color,
+  flexbox,
+  grid,
+  layout,
+  position,
+  shadow,
+  space,
+  typography,
+} from 'styled-system';
 
 const StyledText = styled('p')<any>(
   {
@@ -7,15 +19,31 @@ const StyledText = styled('p')<any>(
   },
   props => ({
     ...props.theme.Text,
-  })
+  }),
+  color,
+  space,
+  flexbox,
+  grid,
+  layout,
+  position,
+  shadow,
+  background,
+  border,
+  typography
 );
 
 export interface TextProps {
   children?: React.ReactNode;
+  lineHeight?: string;
+  props?: any;
 }
 
-const Text = ({ children }: TextProps) => {
-  return <StyledText>{children}</StyledText>;
+const Text = ({ children, lineHeight = 'normal', ...props }: TextProps) => {
+  return (
+    <StyledText lineHeight={lineHeight} {...props}>
+      {children}
+    </StyledText>
+  );
 };
 
 export default Text;
