e058c14d42f5df503e9a5380b15b34927c002b33
Matt Wood <matt@cratebind.com>
2020-02-07 16:33:59 -0600

    fix(theme): add default theme as export

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ export { default as Checkbox } from './Checkbox';
 export { default as Text } from './Text';
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as GlobalStyles } from './GlobalStyles';
+export { default as defaultTheme } from './theme';
 export { Block, Flex } from './layout';
