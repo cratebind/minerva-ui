edc156c8162dd95a725a504fe9b3181326d428e5
Matt Wood <matt@cratebind.com>
2020-03-04 16:35:19 -0600

    feat: add Table and Box component, fix Button component

--- test/link.test.tsx
+++ test/link.test.tsx
@@ -25,6 +25,35 @@ describe('<Link />', () => {
     expect(container).toHaveTextContent(content);
   });
 
+  it('should add attributes when `isExternal` prop is passed', () => {
+    const content = 'Hello';
+    const { getByText } = render(
+      <ThemeProvider>
+        <Link isExternal>{content}</Link>
+      </ThemeProvider>
+    );
+
+    const element = getByText(content);
+
+    expect(element.getAttribute('rel')).toEqual('noopener noreferrer');
+    expect(element.getAttribute('target')).toEqual('_blank');
+  });
+
+  it('should change styles when `isDisabled` prop is passed', () => {
+    const content = 'Hello';
+    const { debug, getByText } = render(
+      <ThemeProvider>
+        <Link isDisabled>{content}</Link>
+      </ThemeProvider>
+    );
+
+    const element = getByText(content);
+
+    debug(element);
+
+    expect(element).toHaveStyleRule('cursor', 'not-allowed');
+  });
+
   // it('should show new background color if provided in theme', () => {
   //   const newColor = 'green';
   //   const theme = {
