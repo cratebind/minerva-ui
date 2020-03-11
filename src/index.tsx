7cbee67517a5865752e698c54adc993247cd25e3
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-03-10 22:57:35 -0500

    Uncomment button tests, try named Button export for pipeline failure

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ export { default as PseudoBox } from './PseudoBox';
 /**
  * Inputs
  */
-export { default as Button } from './Button';
+export { Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Input } from './Input';
 export { default as InputField } from './InputField';
