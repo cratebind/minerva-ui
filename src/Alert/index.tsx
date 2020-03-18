3c1e208c04bbddf67e7d7520926d6c8e1cb37c12
Bekah Baker <rebekah@cratebind.com>
2020-03-18 15:27:15 -0500

    Added colors for different status codes

--- src/Alert/index.tsx
+++ src/Alert/index.tsx
@@ -1,14 +1,12 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
-import { MinervaProps, systemProps, Box } from '../layout';
+import { MinervaProps, systemProps, Flex } from '../layout';
 
-const StyledAlert = styled(Box)<MinervaProps>(
+const StyledAlert = styled(Flex)(
   props => ({
     ...props.theme.Alert,
-    display: 'flex',
     padding: '16px 8px',
-    backgroundColor: '#f8b4b4',
-    // TODO change background color based on status
+    backgroundColor: props.statusColor,
   }),
   systemProps
 );
@@ -23,8 +21,6 @@ const StyledAlertTitle = styled('p')(
 
 const StyledAlertDescription = styled.p``;
 
-// TODO add icon and change based on status
-
 export interface AlertProps extends MinervaProps {
   children?: React.ReactNode;
   status?: string;
@@ -37,8 +33,25 @@ export const Alert = forwardRef(function Alert(
   { title, description, status, ...props }: AlertProps,
   ref
 ) {
+  var statusColor;
+  switch (status) {
+    case 'error':
+      statusColor = '#f8b4b4';
+      break;
+    case 'success':
+      statusColor = '#bcf0da';
+      break;
+    case 'warning':
+      statusColor = '#fdf6b2';
+      break;
+    case 'info':
+      statusColor = '#c3ddfd';
+      break;
+    default:
+      statusColor = '#e5e7eb';
+  }
   return (
-    <StyledAlert ref={ref} {...props} status={status}>
+    <StyledAlert ref={ref} {...props} statusColor={statusColor}>
       <StyledAlertTitle>{title}</StyledAlertTitle>
       <StyledAlertDescription>{description}</StyledAlertDescription>
     </StyledAlert>
