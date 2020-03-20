858f12d403f6fe75af9f81000abf82ff36098f59
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-03-20 17:39:41 -0500

    WIP: radio buttons work fine, able to add custom styles, accessibility wip

--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -1,68 +1,115 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { MinervaProps, systemProps } from '../layout';
-import Input from '../Input';
 import { Box } from '../layout';
+import VisuallyHidden from '../VisuallyHidden';
 
 const RadioGroupContainer = styled(Box)({}, systemProps);
 
-const Container = styled.div`
-  display: flex;
-`;
+const RadioContainer = styled(Box)<RadioProps>(
+  (props: RadioProps) => ({
+    display: 'flex',
+    alignItems: 'center',
+    margin: '0 10px 10px 0',
+  }),
+  systemProps
+);
 
-const StyledRadio = styled(Input)`
-  width: max-content;
-`;
-
+const ControlBox = styled(Box)<ControlBoxProps>((props: ControlBoxProps) => ({
+  display: 'inline-block',
+  width: props.radioSize,
+  height: props.radioSize,
+  padding: '0.1em',
+  marginRight: '8px',
+  backgroundClip: 'content-box',
+  border: props.checked
+    ? `${parseInt(props.radioSize) / 3}px solid ${props.radioColor}`
+    : '2px solid #ecebed',
+  borderRadius: '50%',
+  transition: 'background-color 120ms ease 0s',
+  ':focus': {
+    borderColor: '#a4cafe',
+    boxShadow: '0 0 0 3px rgba(118,169,250,.45)',
+    outline: 0,
+  },
+}));
 export interface RadioGroupProps {
   children?: React.ReactNode;
   selectedValue?: string;
+  radioColor?: string;
+  radioSize?: string;
 }
 
-const SelectedValueContext = React.createContext<string | undefined>(undefined);
+export type ControlBoxProps = {
+  checked?: boolean;
+  theme?: any;
+  radioColor?: string;
+  radioSize?: any;
+};
+
+export interface RadioProps extends BaseProps {
+  children?: React.ReactNode;
+  value?: string;
+  isDisabled?: boolean;
+}
+
+const SelectedValueContext = React.createContext<string | undefined | any>(
+  undefined
+);
+
 export const RadioGroup = ({
   children,
   selectedValue,
+  radioSize,
+  radioColor,
   ...props
 }: RadioGroupProps) => {
   return (
-    <SelectedValueContext.Provider value={selectedValue}>
-      <RadioGroupContainer {...props}>{children}</RadioGroupContainer>
+    <SelectedValueContext.Provider
+      value={{ selectedValue, radioColor, radioSize, ...props }}
+    >
+      <RadioGroupContainer role="radiogroup" {...props}>
+        {children}
+      </RadioGroupContainer>
     </SelectedValueContext.Provider>
   );
 };
 
 type BaseProps = MinervaProps & React.InputHTMLAttributes<HTMLInputElement>;
 
-export interface RadioProps extends BaseProps {
-  children?: React.ReactNode;
-  value?: string;
-  isDisabled?: boolean;
-  defaultChecked?: boolean;
-}
-
 export const Radio = ({
   value,
   isDisabled,
-  defaultChecked, // @TODO: add defaultValue logic
   children,
   ...props
 }: RadioProps) => {
-  const selectedValue = useContext(SelectedValueContext);
+  const { selectedValue, radioColor, radioSize } = useContext(
+    SelectedValueContext
+  );
   return (
-    <Container>
-      <StyledRadio
-        mr="10px"
-        mb="10px"
-        type="radio"
-        id={value}
-        value={value}
-        checked={value === selectedValue}
-        disabled={isDisabled}
-        {...props}
-      />
-      <label>{children}</label>
-    </Container>
+    <RadioContainer htmlFor={value} as="label" {...props}>
+      <Box display="inline-flex">
+        <VisuallyHidden
+          as="input"
+          type="radio"
+          aria-label={value}
+          id={value}
+          value={value}
+          checked={value === selectedValue}
+          disabled={isDisabled}
+        ></VisuallyHidden>
+
+        <ControlBox
+          as="span"
+          checked={value === selectedValue}
+          aria-checked={value === selectedValue}
+          radioColor={radioColor || '#5850ec'}
+          radioSize={radioSize || '16px'}
+          tabIndex={value === selectedValue ? 0 : -1}
+        ></ControlBox>
+      </Box>
+      {children && <Box>{children}</Box>}
+    </RadioContainer>
   );
 };
 
