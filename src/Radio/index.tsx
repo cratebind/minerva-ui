51789a47d85aa6815682e86dd935d7d2cac3a9b2
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-03-23 12:10:40 -0500

    fix: fix radio btn focus issue when using up/down arrow keys

--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -6,14 +6,16 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const RadioGroupContainer = styled(Box)({}, systemProps);
 
-const RadioContainer = styled(Box)<RadioProps>(
-  (props: RadioProps) => ({
-    display: 'flex',
-    alignItems: 'center',
-    margin: '0 10px 10px 0',
-  }),
-  systemProps
-);
+const RadioContainer = styled(Box)`
+  display: flex;
+  align-items: center;
+  margin: 0 10px 10px 0;
+
+  input[type='radio']:focus + [role='radio'] {
+    box-shadow: 0 0 0 3px rgba(118, 169, 250, 0.45);
+  }
+  ${systemProps}
+`;
 
 const ControlBox = styled(Box)<ControlBoxProps>((props: ControlBoxProps) => ({
   display: 'inline-block',
@@ -26,13 +28,8 @@ const ControlBox = styled(Box)<ControlBoxProps>((props: ControlBoxProps) => ({
     ? `${parseInt(props.radioSize) / 3}px solid ${props.radioColor}`
     : '2px solid #ecebed',
   borderRadius: '50%',
-  transition: 'background-color 120ms ease 0s',
-  ':focus': {
-    borderColor: '#a4cafe',
-    boxShadow: '0 0 0 3px rgba(118,169,250,.45)',
-    outline: 0,
-  },
 }));
+
 export interface RadioGroupProps {
   children?: React.ReactNode;
   selectedValue?: string;
@@ -53,9 +50,17 @@ export interface RadioProps extends BaseProps {
   isDisabled?: boolean;
 }
 
-const SelectedValueContext = React.createContext<string | undefined | any>(
-  undefined
-);
+type ContextValue = {
+  selectedValue: string | undefined;
+  radioSize: string | undefined;
+  radioColor: string | undefined;
+};
+
+const SelectedValueContext = React.createContext<ContextValue>({
+  selectedValue: undefined,
+  radioSize: undefined,
+  radioColor: undefined,
+});
 
 export const RadioGroup = ({
   children,
@@ -66,7 +71,7 @@ export const RadioGroup = ({
 }: RadioGroupProps) => {
   return (
     <SelectedValueContext.Provider
-      value={{ selectedValue, radioColor, radioSize, ...props }}
+      value={{ selectedValue, radioColor, radioSize }}
     >
       <RadioGroupContainer role="radiogroup" {...props}>
         {children}
@@ -96,16 +101,16 @@ export const Radio = ({
           id={value}
           value={value}
           checked={value === selectedValue}
+          aria-checked={value === selectedValue}
           disabled={isDisabled}
         ></VisuallyHidden>
 
         <ControlBox
           as="span"
+          role="radio"
           checked={value === selectedValue}
-          aria-checked={value === selectedValue}
           radioColor={radioColor || '#5850ec'}
           radioSize={radioSize || '16px'}
-          tabIndex={value === selectedValue ? 0 : -1}
         ></ControlBox>
       </Box>
       {children && <Box>{children}</Box>}
