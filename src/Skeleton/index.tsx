6242edc0ddc227b5f6f506271089165177b25109
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-03-24 13:10:54 -0500

    WIP: skeleton wip

---
+++ src/Skeleton/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MinervaProps } from '../layout';
+import { useTheme } from '../theme';
+
+const SSkeletonPulse = styled.div`
+  display: inline-block;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(-90deg, #f0f0f0 0%, #f8f8f8 50%, #f0f0f0 100%);
+  background-size: 400% 400%;
+  animation: pulse 1.2s ease-in-out infinite;
+  @keyframes pulse {
+    0% {
+      background-position: 0% 0%;
+    }
+    100% {
+      background-position: -135% 0%;
+    }
+  }
+`;
+
+const SSkeletonLine = styled(SSkeletonPulse)`
+  width: 5.5em;
+  border-radius: 5px;
+
+  &::before {
+    content: '\00a0';
+  }
+`;
+
+export const Skeleton = () => <SSkeletonLine />;
+
+export default Skeleton;
--- src/index.tsx
+++ src/index.tsx
@@ -40,6 +40,7 @@ export { default as Image } from './Image';
 export { default as Table } from './Table';
 export { default as Icon } from './Icon';
 export * from './Table';
+export { default as Skeleton } from './Skeleton';
 
 /**
  * Helpers
