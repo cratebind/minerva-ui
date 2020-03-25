d58038dc9d2ecf8eded1cd28c47eca944a2f0d89
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-03-25 15:44:17 -0500

    WIP: skeleton styles / props wip

--- src/Skeleton/index.tsx
+++ src/Skeleton/index.tsx
@@ -3,10 +3,29 @@ import styled from 'styled-components';
 import { MinervaProps } from '../layout';
 import { useTheme } from '../theme';
 
-const SSkeletonPulse = styled.div`
-  display: inline-block;
-  height: 100%;
+export interface SkeletonProps extends MinervaProps {
+  count?: number;
+  height?: string;
+  width?: string;
+  circle?: boolean;
+}
+
+const Container = styled.span`
+  display: flex;
+  flex-direction: column;
+  height: 100%; 
   width: 100%;
+`;
+
+export interface SkeletonAnimationProps {
+  circle?: boolean;
+}
+
+const SkeletonAnimation = styled('div')`
+  display: inline-block;
+  width: ${(props: SkeletonAnimationProps) => props.circle ? '100%' : '100%' };
+  height: ${(props: SkeletonAnimationProps) => props.circle ? '100%' : '2em' };
+  /* width: 3em; */
   background: linear-gradient(-90deg, #f0f0f0 0%, #f8f8f8 50%, #f0f0f0 100%);
   background-size: 400% 400%;
   animation: pulse 1.2s ease-in-out infinite;
@@ -18,17 +37,36 @@ const SSkeletonPulse = styled.div`
       background-position: -135% 0%;
     }
   }
-`;
-
-const SSkeletonLine = styled(SSkeletonPulse)`
-  width: 5.5em;
+  /* width: 5.5em; */
   border-radius: 5px;
+  line-height: 1;
 
   &::before {
     content: '\00a0';
   }
 `;
 
-export const Skeleton = () => <SSkeletonLine />;
+export const Skeleton = ({
+  count = 1,
+  height,
+  width,
+  circle,
+  ...props
+}: SkeletonProps) => {
+  const theme = useTheme();
+  const elements: JSX.Element[] = [];
+  for (let i = 0; i < count; i++) {
+    elements.push(
+      <SkeletonAnimation
+        style={{ height, width, borderRadius: circle ? '50%' : null }}
+        count={count}
+        circle={circle}
+        {...props}
+        {...theme.Skeleton}
+      ></SkeletonAnimation>
+    );
+  }
+  return <Container {...props} style={{height, width}}>{elements}</Container>;
+};
 
 export default Skeleton;
