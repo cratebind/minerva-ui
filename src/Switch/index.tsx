6f3aca14e305b48f61c889aed3817965ec2154f6
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-04-01 12:21:43 -0500

    WIP: fix switch styles wip

--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -8,23 +8,36 @@ const switchSizes = {
   lg: {
     width: '55px',
     height: '25px',
+    switchBtn: {
+      width: '20px',
+      height: '20px',
+      // margin: '2.5px 1px 2.5px 1px;',
+      margin: '2.5px 4px 2.5px 1px',
+    },
   },
   md: {
     width: '40px',
     height: '18px',
+    switchBtn: {
+      width: '14px',
+      height: '14px',
+      margin: '1.5px 3px',
+    },
   },
   sm: {
     width: '33px',
     height: '14.5px',
+    switchBtn: {
+      width: '11px',
+      height: '11px',
+      margin: '1.5px 3px',
+    },
   },
 };
 
 const SwitchContainer = styled(Box)<SwitchProps>`
   position: relative;
-  width: ${props =>
-    props.switchSize
-      ? switchSizes[props.switchSize].width
-      : switchSizes['md'].width};
+  width: ${props => switchSizes[props.switchSize || 'md'].width};
   display: inline-block;
   vertical-align: middle;
   text-align: left;
@@ -38,9 +51,7 @@ const ControlBox = styled(Box)<SwitchProps>((props: SwitchProps) => ({
   border: '0 solid #ccc',
   borderRadius: '20px',
   margin: '0',
-  height: props.switchSize
-    ? switchSizes[props.switchSize].height
-    : switchSizes['md'].height,
+  height: switchSizes[props.switchSize || 'md'].height,
 
   ':focus-within': {
     borderColor: '#a4cafe',
@@ -51,58 +62,26 @@ const ControlBox = styled(Box)<SwitchProps>((props: SwitchProps) => ({
 
 const SwitchInner = styled.span<CustomSwitchProps>`
   display: block;
-  width: 200%;
-  /* margin-left: -100%; */
-  transition: transform 250ms ease 2s;
-  transform: ${props =>
-    props.checked ? 'translateX(100%)' : 'translateX(0%)'};
-
-  &::before,
-  &::after {
-    display: block;
-    float: left;
-    width: 50%;
-    height: 34px;
-    padding: 0;
-    line-height: 34px;
-    font-size: 14px;
-    color: white;
-    font-weight: bold;
-    box-sizing: border-box;
-  }
-
-  &::before {
-    content: '';
-    padding-left: 10px;
-    background-color: ${props =>
-      props.switchColor ? props.switchColor : '#5850ec'};
-    color: #fff;
-  }
-
-  &::after {
-    content: '';
-    padding-right: 10px;
-    background-color: #ccc;
-    color: #fff;
-    text-align: right;
-  }
+  background-color: ${props => (props.checked ? props.switchColor : '#ccc')};
+  height: 34px;
 `;
 
 const StyledSwitch = styled.span<CustomSwitchProps>`
   display: block;
-  width: 24px;
-  margin: 5px;
   background: #fff;
   position: absolute;
-  top: 0;
   bottom: 0;
   border: 0 solid #ccc;
   border-radius: 50%;
-  transition: right 0.2s ease-in 0s;
-  right: ${props => (props.checked ? '0%' : '56%')};
-  margin: 5.3%;
+  transition: transform 0.2s ease-in;
+  transform: ${props =>
+    props.checked ? 'translateX(150%)' : 'translateX(0%)'};
+  /* margin: 5%;
   width: 34%;
-  height: 76%;
+  height: 76%; */
+  margin: ${props => switchSizes[props.switchSize || 'md'].switchBtn.margin};
+  width: ${props => switchSizes[props.switchSize || 'md'].switchBtn.width};
+  height: ${props => switchSizes[props.switchSize || 'md'].switchBtn.height};
 `;
 
 type BaseProps = MinervaProps & React.InputHTMLAttributes<HTMLInputElement>;
@@ -120,6 +99,7 @@ export interface SwitchProps extends BaseProps {
 export interface CustomSwitchProps {
   checked?: boolean;
   switchColor?: string;
+  switchSize?: string;
 }
 
 export const Switch = forwardRef(function Checkbox(
@@ -128,8 +108,8 @@ export const Switch = forwardRef(function Checkbox(
 ) {
   const {
     checked = false,
-    switchSize,
-    switchColor,
+    switchSize = 'md',
+    switchColor = '#5850ec',
     onChange,
     htmlFor,
     ...rest
@@ -153,7 +133,7 @@ export const Switch = forwardRef(function Checkbox(
             switchColor={switchColor}
             data-testid="switch-inner"
           />
-          <StyledSwitch checked={checked} data-testid="switch" />
+          <StyledSwitch checked={checked} switchSize={switchSize} data-testid="switch" />
         </ControlBox>
       </CustomCheckboxContainer>
     </SwitchContainer>
