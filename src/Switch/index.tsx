21887b9e9c6367864ac073a4121f76a35bc01efc
Matt Wood <matt@cratebind.com>
2020-04-02 09:13:53 -0500

    fix: remove unused parameter

--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -4,7 +4,7 @@ import { CustomCheckboxContainer, CustomCheckboxInput } from '@reach/checkbox';
 import { MinervaProps, Box, systemProps } from '../layout';
 import VisuallyHidden from '../VisuallyHidden';
 
-const SwitchContainer = styled(Box)<SwitchProps>`
+const SwitchContainer = styled(Box) <SwitchProps>`
   position: relative;
   display: inline-block;
   vertical-align: middle;
@@ -12,7 +12,7 @@ const SwitchContainer = styled(Box)<SwitchProps>`
   ${systemProps}
 `;
 
-const ControlBox = styled(Box)<SwitchProps>((props: SwitchProps) => ({
+const ControlBox = styled(Box)<SwitchProps>(() => ({
   display: 'block',
   overflow: 'hidden',
   cursor: 'pointer',
