573db0162d91d07e760ac58dbd24ee3f2e1bea96
Matt Wood <matt@cratebind.com>
2020-09-30 20:20:52 -0500

    fix: remove unused code and redundant comments from global styles

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -98,11 +98,8 @@ export const ReachStyles = css`
   [data-reach-menu-items] {
     display: block;
     white-space: nowrap;
-    /* border: solid 1px hsla(0, 0%, 0%, 0.25); */
     background: hsla(0, 100%, 100%, 0.99);
     outline: none;
-    /* padding: 1rem 0; */
-    /* font-size: 85%; */
   }
 
   [data-reach-menu-item] {
@@ -110,49 +107,23 @@ export const ReachStyles = css`
     user-select: none;
   }
 
-  /*
- The dom structure of a MenuLink is reach-menu-item > a,
- so to target all items we can use 'data-reach - menu - item'
-*/
   [data-reach-menu-item] {
-    /*
-    These are styled in one rule instead of something like a[data-reach-menu-item]
-    and li[data-reach-menu-item] so that apps don't have to fight specificity and
-    can style both li and a menu items with one rule,
-    ie: '[data - selected] { background: red; } '.
-    Otherwise they'd have to define two styles, one for a and one for li.
-  */
-
-    /* reach-menu-item */
     cursor: pointer;
 
-    /* a */
     display: block;
-    /* color: inherit; */
-    /* font: inherit; */
     text-decoration: initial;
-
-    /* both */
-    /* padding: 5px 20px; */
   }
 
-  /* pseudo pseudo selector */
   [data-reach-menu-item][data-selected] {
-    /* background: hsl(211, 81%, 36%); */
-    /* color: white; */
     outline: none;
   }
 
-  /*
- * Reach Dialog
- */
-
+  /* Reach Dialog */
   :root {
     --reach-dialog: 1;
   }
 
   [data-reach-dialog-overlay] {
-    /* background: hsla(0, 0%, 0%, 0.33); */
     position: fixed;
     top: 0;
     right: 0;
@@ -162,16 +133,10 @@ export const ReachStyles = css`
   }
 
   [data-reach-dialog-content] {
-    /* width: 50vw; */
-    /* margin: 10vh auto; */
-    /* background: white; */
-    /* padding: 2rem; */
     outline: none;
   }
 
-  /**
- * Reach Tabs
- */
+  /* Reach Tabs */
   :root {
     --reach-tabs: 1;
   }
@@ -189,34 +154,16 @@ export const ReachStyles = css`
   }
 
   [data-reach-tab] {
-    /* display: inline-block; */
-    /* border: none; */
-    /* padding: 0.25em 0.5em; */
-    /* margin: 0; */
-    /* background: none; */
-    /* color: inherit; */
-    /* font: inherit; */
     cursor: pointer;
     -webkit-appearance: none;
     -moz-appearance: none;
   }
 
-  [data-reach-tab]:active {
-    /* background: hsla(0, 0%, 0%, 0.05); */
-  }
-
   [data-reach-tab]:disabled {
-    /* opacity: 0.25; */
     cursor: default;
   }
 
-  [data-reach-tab][data-selected] {
-    /* border-bottom: 1px solid currentColor; */
-  }
-
-  /**
-   * Reach Tooltip
-   */
+  /* Reach Tooltip */
   :root {
     --reach-tooltip: 1;
   }
@@ -229,9 +176,7 @@ export const ReachStyles = css`
     animation: grow 0.1s ease;
   }
 
-  /**
-   * Reach Combobox
-   */
+  /* Reach Combobox */
   :root {
     --reach-combobox: 1;
   }
@@ -275,97 +220,45 @@ export const ReachStyles = css`
 /**
  * Base styles from Tailwind CSS
  * https://tailwindcss.com/docs/adding-base-styles/#app
+ *
+ * Skip including Reach UI styles since we're inlining them
+ * https://reacttraining.com/reach-ui/styling/#skip-including-styles
  */
 const GlobalStyles = createGlobalStyle`
-/**
-Skip including Reach UI styles since we're inlining them
-https://reacttraining.com/reach-ui/styling/#skip-including-styles
- */
 
 :root {
   --reach-checkbox: 1;
 }
-/* Document
-   ========================================================================== */
-
-/**
- * 1. Correct the line height in all browsers.
- * 2. Prevent adjustments of font size after orientation changes in iOS.
- */
-
 html {
   line-height: 1.15; /* 1 */
   -webkit-text-size-adjust: 100%; /* 2 */
 }
 
-/* Sections
-   ========================================================================== */
-
-/**
- * Remove the margin in all browsers.
- */
-
 body {
   margin: 0;
 }
-
-/**
- * Render the 'main' element consistently in IE.
- */
-
 main {
   display: block;
 }
-
-/**
- * Correct the font size and margin on 'h1' elements within 'section' and
- * 'article' contexts in Chrome, Firefox, and Safari.
- */
 
 h1 {
   font-size: 2em;
   margin: 0.67em 0;
 }
-
-/* Grouping content
-   ========================================================================== */
-
-/**
- * 1. Add the correct box sizing in Firefox.
- * 2. Show the overflow in Edge and IE.
- */
-
 hr {
   box-sizing: content-box; /* 1 */
   height: 0; /* 1 */
   overflow: visible; /* 2 */
 }
 
-/**
- * 1. Correct the inheritance and scaling of font size in all browsers.
- * 2. Correct the odd 'em' font sizing in all browsers.
- */
-
 pre {
   font-family: monospace, monospace; /* 1 */
   font-size: 1em; /* 2 */
 }
 
-/* Text-level semantics
-   ========================================================================== */
-
-/**
- * Remove the gray background on active links in IE 10.
- */
-
 a {
   background-color: transparent;
 }
-
-/**
- * 1. Remove the bottom border in Chrome 57-
- * 2. Add the correct text decoration in Chrome, Edge, IE, Opera, and Safari.
- */
 
 abbr[title] {
   border-bottom: none; /* 1 */
@@ -373,20 +266,10 @@ abbr[title] {
   -webkit-text-decoration: underline dotted;
           text-decoration: underline dotted; /* 2 */
 }
-
-/**
- * Add the correct font weight in Chrome, Edge, and Safari.
- */
-
 b,
 strong {
   font-weight: bolder;
 }
-
-/**
- * 1. Correct the inheritance and scaling of font size in all browsers.
- * 2. Correct the odd 'em' font sizing in all browsers.
- */
 
 code,
 kbd,
@@ -395,18 +278,9 @@ samp {
   font-size: 1em; /* 2 */
 }
 
-/**
- * Add the correct font size in all browsers.
- */
-
 small {
   font-size: 80%;
 }
-
-/**
- * Prevent 'sub' and 'sup' elements from affecting the line height in
- * all browsers.
- */
 
 sub,
 sup {
@@ -424,24 +298,9 @@ sup {
   top: -0.5em;
 }
 
-/* Embedded content
-   ========================================================================== */
-
-/**
- * Remove the border on images inside links in IE 10.
- */
-
 img {
   border-style: none;
 }
-
-/* Forms
-   ========================================================================== */
-
-/**
- * 1. Change the font styles in all browsers.
- * 2. Remove the margin in Firefox and Safari.
- */
 
 button,
 input,
@@ -454,29 +313,14 @@ textarea {
   margin: 0; /* 2 */
 }
 
-/**
- * Show the overflow in IE.
- * 1. Show the overflow in Edge.
- */
-
 button,
 input { /* 1 */
   overflow: visible;
 }
-
-/**
- * Remove the inheritance of text transform in Edge, Firefox, and IE.
- * 1. Remove the inheritance of text transform in Firefox.
- */
-
 button,
 select { /* 1 */
   text-transform: none;
 }
-
-/**
- * Correct the inability to style clickable types in iOS and Safari.
- */
 
 button,
 [type="button"],
@@ -484,10 +328,6 @@ button,
 [type="submit"] {
   -webkit-appearance: button;
 }
-
-/**
- * Remove the inner border and padding in Firefox.
- */
 
 button::-moz-focus-inner,
 [type="button"]::-moz-focus-inner,
@@ -497,10 +337,6 @@ button::-moz-focus-inner,
   padding: 0;
 }
 
-/**
- * Restore the focus styles unset by the previous rule.
- */
-
 button:-moz-focusring,
 [type="button"]:-moz-focusring,
 [type="reset"]:-moz-focusring,
@@ -508,20 +344,9 @@ button:-moz-focusring,
   outline: 1px dotted ButtonText;
 }
 
-/**
- * Correct the padding in Firefox.
- */
-
 fieldset {
   padding: 0.35em 0.75em 0.625em;
 }
-
-/**
- * 1. Correct the text wrapping in Edge and IE.
- * 2. Correct the color inheritance from 'fieldset' elements in IE.
- * 3. Remove the padding so developers are not caught out when they zero out
- *    'fieldset' elements in all browsers.
- */
 
 legend {
   box-sizing: border-box; /* 1 */
@@ -532,26 +357,13 @@ legend {
   white-space: normal; /* 1 */
 }
 
-/**
- * Add the correct vertical alignment in Chrome, Firefox, and Opera.
- */
-
 progress {
   vertical-align: baseline;
 }
 
-/**
- * Remove the default vertical scrollbar in IE 10+.
- */
-
 textarea {
   overflow: auto;
 }
-
-/**
- * 1. Add the correct box sizing in IE 10.
- * 2. Remove the padding in IE 10.
- */
 
 [type="checkbox"],
 [type="radio"] {
@@ -559,90 +371,40 @@ textarea {
   padding: 0; /* 2 */
 }
 
-/**
- * Correct the cursor style of increment and decrement buttons in Chrome.
- */
-
 [type="number"]::-webkit-inner-spin-button,
 [type="number"]::-webkit-outer-spin-button {
   height: auto;
 }
-
-/**
- * 1. Correct the odd appearance in Chrome and Safari.
- * 2. Correct the outline style in Safari.
- */
 
 [type="search"] {
   -webkit-appearance: textfield; /* 1 */
   outline-offset: -2px; /* 2 */
 }
 
-/**
- * Remove the inner padding in Chrome and Safari on macOS.
- */
-
 [type="search"]::-webkit-search-decoration {
   -webkit-appearance: none;
 }
-
-/**
- * 1. Correct the inability to style clickable types in iOS and Safari.
- * 2. Change font properties to 'inherit' in Safari.
- */
 
 ::-webkit-file-upload-button {
   -webkit-appearance: button; /* 1 */
   font: inherit; /* 2 */
 }
 
-/* Interactive
-   ========================================================================== */
-
-/*
- * Add the correct display in Edge, IE 10+, and Firefox.
- */
-
 details {
   display: block;
 }
-
-/*
- * Add the correct display in all browsers.
- */
 
 summary {
   display: list-item;
 }
 
-/* Misc
-   ========================================================================== */
-
-/**
- * Add the correct display in IE 10+.
- */
-
 template {
   display: none;
 }
 
-/**
- * Add the correct display in IE 10.
- */
-
 [hidden] {
   display: none;
 }
-
-/**
- * Manually forked from SUIT CSS Base: https://github.com/suitcss/base
- * A thin layer on top of normalize.css that provides a starting point more
- * suitable for web applications.
- */
-
-/**
- * Removes the default spacing and border for appropriate elements.
- */
 
 blockquote,
 dl,
@@ -666,11 +428,6 @@ button {
   padding: 0;
 }
 
-/**
- * Work around a Firefox/IE bug where the transparent 'button' background
- * results in a loss of the default 'button' focus styles.
- */
-
 button:focus {
   outline: 1px dotted;
   outline: 5px auto -webkit-focus-ring-color;
@@ -688,46 +445,10 @@ ul {
   padding: 0;
 }
 
-/**
- * Tailwind custom reset styles
- */
-
-/**
- * 1. Use the system font stack as a sane default.
- * 2. Use Tailwind's default "normal" line-height so the user isn't forced
- *    to override it to ensure consistency even when using the default theme.
- */
-
 html {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; /* 1 */
   line-height: 1.5; /* 2 */
 }
-
-/**
- * 1. Prevent padding and border from affecting element width.
- *
- *    We used to set this in the html element and inherit from
- *    the parent element for everything else. This caused issues
- *    in shadow-dom-enhanced elements like <details> where the content
- *    is wrapped by a div with box-sizing set to 'content-box'.
- *
- *    https://github.com/mozdevs/cssremedy/issues/4
- *
- *
- * 2. Allow adding a border to an element by just adding a border-width.
- *
- *    By default, the way the browser specifies that an element should have no
- *    border is by setting it's border-style to 'none' in the user-agent
- *    stylesheet.
- *
- *    In order to easily add borders to elements by just setting the 'border-width'
- *    property, we change the default border-style for all elements to 'solid', and
- *    use border-width to hide them instead. This way our 'border' utilities only
- *    need to set the 'border-width' property instead of the entire 'border'
- *    shorthand, making our border utilities much more straightforward to compose.
- *
- *    https://github.com/tailwindcss/tailwindcss/pull/116
- */
 
 *,
 ::before,
@@ -738,23 +459,9 @@ html {
   border-color: #d2d6dc; /* 2 */
 }
 
-/*
- * Ensure horizontal rules are visible by default
- */
-
 hr {
   border-top-width: 1px;
 }
-
-/**
- * Undo the 'border-style: none' reset that Normalize applies to images so that
- * our 'border-{width}' utilities have the expected effect.
- *
- * The Normalize reset is unnecessary for us since we default the border-width
- * to 0 on all elements.
- *
- * https://github.com/tailwindcss/tailwindcss/issues/362
- */
 
 img {
   border-style: solid;
@@ -796,23 +503,10 @@ h6 {
   font-weight: inherit;
 }
 
-/**
- * Reset links to optimize for opt-in styling instead of
- * opt-out.
- */
-
 a {
   color: inherit;
   text-decoration: inherit;
 }
-
-/**
- * Reset form element properties that are easy to forget to
- * style explicitly so you don't inadvertently introduce
- * styles that deviate from your design system. These styles
- * supplement a partial reset that is already applied by
- * normalize.css.
- */
 
 button,
 input,
@@ -824,27 +518,12 @@ textarea {
   color: inherit;
 }
 
-/**
- * Use the configured 'mono' font family for elements that
- * are expected to be rendered with a monospace font, falling
- * back to the system monospace stack if there is no configured
- * 'mono' font family.
- */
-
 pre,
 code,
 kbd,
 samp {
   font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
 }
-
-/**
- * Make replaced elements 'display: block' by default as that's
- * the behavior you want almost all of the time. Inspired by
- * CSS Remedy, with 'svg' added as well.
- *
- * https://github.com/mozdevs/cssremedy/issues/14
- */
 
 img,
 svg,
@@ -857,13 +536,6 @@ object {
   display: block;
   vertical-align: middle;
 }
-
-/**
- * Constrain images and videos to the parent width and preserve
- * their intrinsic aspect ratio.
- *
- * https://github.com/mozdevs/cssremedy/issues/14
- */
 
 img,
 video {
