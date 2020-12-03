5218930c859a6f89cc3fa71e6076b29c28dec853
Matt Wood <matt@cratebind.com>
2020-12-03 16:40:33 -0600

    Standardize font families across platforms

--- cypress/components/button.spec.tsx
+++ cypress/components/button.spec.tsx
@@ -2,13 +2,51 @@
 
 import React from 'react';
 import { mount } from 'cypress-react-unit-test';
-import { Button, ThemeProvider } from '../../dist/minerva-ui.esm';
+import {
+  Button,
+  ThemeProvider,
+  GlobalStyles,
+  defaultTheme,
+} from '../../dist/minerva-ui.esm';
+import { createGlobalStyle } from 'styled-components';
 
 const text = 'Button';
 
+// by default, we are using the native font stack
+// but this font is different on macOS, Linux and Windows
+// to make sure our screenshots are consistent, we force them all to use the same font family
+const StandardizeFont = createGlobalStyle`
+  html {
+    font-family: Helvetica;
+  }
+`;
+
+const customTheme = {
+  ...defaultTheme,
+  fonts: {
+    ...defaultTheme.fonts,
+    body: 'Helvetica',
+    heading: 'Helvetica',
+  },
+};
+
+const MinervaProvider = ({ children, theme = customTheme }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <StandardizeFont />
+    {children}
+  </ThemeProvider>
+);
+
 describe('<Button />', () => {
-  it('renders and shows inner text', () => {
-    mount(<Button>{text}</Button>);
+  it('renders and shows inner text without a provider', () => {
+    mount(
+      <>
+        <GlobalStyles />
+        <StandardizeFont />
+        <Button>{text}</Button>
+      </>
+    );
 
     cy.contains(text).should('be.visible');
 
@@ -17,9 +55,9 @@ describe('<Button />', () => {
 
   it('renders with a theme provider', () => {
     mount(
-      <ThemeProvider>
+      <MinervaProvider>
         <Button>{text}</Button>
-      </ThemeProvider>
+      </MinervaProvider>
     );
 
     cy.contains(text).should('be.visible');
