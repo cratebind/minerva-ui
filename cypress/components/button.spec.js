1383c0d21018faddbb324c775daed64102242dbc
Matt Wood <matt@cratebind.com>
2020-12-04 12:27:55 -0600

    Update image snapshots and jest snapshots

--- cypress/components/button.spec.js
+++ cypress/components/button.spec.js
@@ -40,35 +40,66 @@ const MinervaProvider = ({ children, theme = customTheme }) => (
 );
 
 describe('<Button />', () => {
-  it('renders with default theme provider', () => {
-    mount(
-      <MinervaProvider>
-        <Button>{text}</Button>
-      </MinervaProvider>
-    );
+  context('Basic Rendering', () => {
+    it('renders with default theme provider', () => {
+      mount(
+        <MinervaProvider>
+          <div id="container">
+            <Button>{text}</Button>
+            <Button variant="secondary">{text}</Button>
+            <Button variant="tertiary">{text}</Button>
+          </div>
+        </MinervaProvider>
+      );
 
-    cy.contains(text).should('be.visible');
-    cy.get('button').toMatchImageSnapshot();
+      cy.contains(text).should('be.visible');
 
-    cy.get('button')
-      .trigger('mouseenter')
-      .toMatchImageSnapshot({ name: 'Themed Button (Click)' });
+      cy.get('#container').toMatchImageSnapshot({
+        name: `Default Theme: Button with Variants`,
+      });
+    });
 
-    // cy.get('button')
-    //   .focus()
-    //   .toMatchImageSnapshot({ name: 'Themed Button (Focus)' });
+    it('renders without default theme', () => {
+      mount(
+        <MinervaProvider
+          theme={{ fonts: { body: 'Helvetica', heading: 'Helvetica' } }}
+        >
+          <Button>{text}</Button>
+        </MinervaProvider>
+      );
+
+      cy.contains(text).should('be.visible');
+      cy.get('button').toMatchImageSnapshot();
+    });
   });
 
-  it('renders without default theme', () => {
-    mount(
-      <MinervaProvider
-        theme={{ fonts: { body: 'Helvetica', heading: 'Helvetica' } }}
-      >
-        <Button>{text}</Button>
-      </MinervaProvider>
-    );
+  context('Style Props', () => {
+    it('should be able to pass basic style props', () => {
+      const color = 'rgb(227, 227, 227)';
+      const backgroundColor = 'rgb(51, 51, 51)';
+      mount(
+        <MinervaProvider>
+          <Button color={color} bg={backgroundColor}>
+            Grey Text Button
+          </Button>
+        </MinervaProvider>
+      );
 
-    cy.contains(text).should('be.visible');
-    cy.get('button').toMatchImageSnapshot();
+      cy.get('button').should('have.css', 'color', color);
+      cy.get('button').should('have.css', 'background-color', backgroundColor);
+    });
+
+    it('should be able to use pseudo style props', () => {
+      const backgroundColor = 'rgb(227, 227, 227)';
+      mount(
+        <MinervaProvider>
+          <Button _disabled={{ backgroundColor: backgroundColor }} disabled>
+            Grey Text Button
+          </Button>
+        </MinervaProvider>
+      );
+
+      cy.get('button').should('have.css', 'background-color', backgroundColor);
+    });
   });
 });
