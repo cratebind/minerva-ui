48c44d103ba459bc32840caab7e68a2a69657a71
Pedro Marti <pedro@cratebind.com>
2021-06-03 12:08:45 -0400

    fix: fix: storybook example

--- src/Radio/Radio.stories.tsx
+++ src/Radio/Radio.stories.tsx
@@ -4,9 +4,9 @@ import { filteredArgs } from '../utils';
 import { Radio, RadioGroup, RadioProps } from '.';
 
 const RADIO_BUTTONS_EXAMPLE = [
-  { title: 'Meat', value: 'test' },
-  { title: 'Vegetarian', value: 'test1' },
-  { title: 'Supreme', value: 'test2' },
+  { title: 'Radio button label 1', value: 'radio1' },
+  { title: 'Radio button label 2', value: 'radio2' },
+  { title: 'Radio button label 3', value: 'radio3' },
 ];
 
 const meta: Meta = {
@@ -30,7 +30,7 @@ const meta: Meta = {
 export default meta;
 
 const Template: Story<RadioProps> = args => {
-  const [value, setValue] = React.useState('test');
+  const [value, setValue] = React.useState('radio1');
   return (
     <RadioGroup onChange={e => setValue(e.target.value)} value={value}>
       {RADIO_BUTTONS_EXAMPLE.map(({ value, title }) => (
